f307cffa585c2f9c4be9c8d90be2d4cb4f9f03e6
Schumova Maria <schumova.maria@gmail.com>
2025-06-27 14:36:45 +0300

    build: react, scss

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,11 +1,26 @@
 import webpack from 'webpack';
 
+const typescriptLoader = {
+  test: /\.tsx?$/,
+  use: 'ts-loader',
+  exclude: /node_modules/,
+}
+
+const cssLoader = {
+  test: /\.s[ac]ss$/i,
+  use: [
+    // Creates `style` nodes from JS strings
+    "style-loader",
+    // Translates CSS into CommonJS
+    "css-loader",
+    // Compiles Sass to CSS
+    "sass-loader",
+  ],
+}
+
 export function buildLoaders(): webpack.RuleSetRule[] {
     return [
-        {
-          test: /\.tsx?$/,
-          use: 'ts-loader',
-          exclude: /node_modules/,
-        },
+      typescriptLoader,
+      cssLoader,
     ]
 }
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import { someFn } from "./test";
-
-someFn(123);
-
-document.body.innerHTML = 'Hello World';
